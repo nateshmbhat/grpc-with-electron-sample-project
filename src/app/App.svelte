12b910dcb2bc3be2499d91a892af24a044dc6ec6
nateshmbhat <nateshmbhat1@gmail.com>
2020-10-12 15:45:19 +0530

    added all initial configuration. All good to go for DEV !

---
+++ src/app/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let name: string = "hello";
+  import { readdirSync } from "fs";
+  let files: string[] = [];
+
+  const loadFiles = (e) => {
+    files = readdirSync(".");
+  };
+</script>
+
+<style>
+  h1 {
+    color: green;
+  }
+  p {
+    font-family: Lora, Georgia, "Times New Roman", Times, serif;
+    color: blue;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<p>Hello its me sdlkjfdksf</p>
+<h1>sdfsdfds</h1>
+<button on:click|preventDefault={loadFiles}>click me</button>
+<p>{files}</p>
---
+++ src/internals/testing/grpc/client/grpc-test-client.ts
@@ -0,0 +1,31 @@
+var PROTO_PATH = __dirname + "/../protos/greeter-service.proto";
+
+import {loadPackageDefinition , credentials } from 'grpc'
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+var hello_proto = loadPackageDefinition(packageDefinition).helloworld;
+console.log(JSON.stringify(hello_proto))
+
+function main() {
+  var client = new hello_proto['Greeter'](
+    "localhost:50051",
+    credentials.createInsecure()
+  );
+  var user;
+  if (process.argv.length >= 3) {
+    user = process.argv[2];
+  } else {
+    user = "world";
+  }
+  client.sayHello({ name: user }, function (err, response) {
+    console.log("Greeting:", response.message);
+  });
+}
+
+main();
