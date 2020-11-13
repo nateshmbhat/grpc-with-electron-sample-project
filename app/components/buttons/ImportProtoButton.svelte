355e48bb9f8f0331a9e86d60592c2840547f74a1
nateshmbhat <nateshmbhat1@gmail.com>
2020-11-13 21:18:32 +0530

    updated and formatted

--- app/components/buttons/ImportProtoButton.svelte
+++ app/components/buttons/ImportProtoButton.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
-import { pathToFileURL } from "url";
-import { loadProtos } from "../../behaviour/importProtos";
-import path from 'path'
-import { createEventDispatcher } from "svelte";
-import type { ProtoFile } from "../../behaviour/protobuf";
-import {Button} from 'svelte-materialify'
-import { ProtoInfo } from "../../behaviour/protoInfo";
-  //@ts-ignore
-  const SAMPLE_PROT_PATH = path.join( __static , 'sample', 'greeter-service.proto')  
+  import { pathToFileURL } from 'url'
+  import { loadProtos } from '../../behaviour/importProtos'
+  import path from 'path'
+  import { createEventDispatcher } from 'svelte'
+  import type { ProtoFile } from '../../behaviour/protobuf'
+  import { Button } from 'svelte-materialify'
+  import { ProtoInfo } from '../../behaviour/protoInfo'
+  const SAMPLE_PROT_PATH = path.join(
+    //@ts-ignore
+    __static,
+    'sample',
+    'greeter-service.proto',
+  )
 
-  var dispatcher = createEventDispatcher<{protoFiles : ProtoFile[]}>();
+  var dispatcher = createEventDispatcher<{ protoFiles: ProtoFile[] }>()
 
   //@ts-ignore
-  const staticPath= __static;
+  const staticPath = __static
   const importSampleProto = async () => {
-      const   protoFiles                =                       await               loadProtos               ([SAMPLE_PROT_PATH])
-      console.dir(protoFiles);
-      dispatcher('protoFiles' , protoFiles)
-  };
+    const protoFiles = await loadProtos([SAMPLE_PROT_PATH])
+    console.dir(protoFiles)
+    dispatcher('protoFiles', protoFiles)
+  }
 </script>
 
 <Button on:click={importSampleProto}>Import Proto</Button>
--- app/components/editor/RequestEditor.svelte
+++ app/components/editor/RequestEditor.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { AceEditor } from "svelte-ace";
-  require("brace/mode/json");
-  require("brace/theme/chrome");
+  import { AceEditor } from 'svelte-ace'
+  require('brace/mode/json')
+  require('brace/theme/chrome')
 
-  let width = "100";
-  const lang = "json",
-    theme = "chrome";
+  let width = '100'
+  const lang = 'json',
+    theme = 'chrome'
 </script>
 
 <AceEditor {theme} {lang} height="512" />
